05db0c9acfc10d967aef36c7a5ddf3a22ebc6a11
AKuHAK <akuhak@gmail.com>
2018-12-23 18:46:41 +0200

    All files formatted with clang-format

--- ee/example/main.c
+++ ee/example/main.c
@@ -27,77 +27,76 @@ Counters1 myCounter1;
 
 void loadModules(void)
 {
-    int ret;
+	int ret;
 
-    ret = SifLoadModule("rom0:LIBSD",0,NULL);
-    if ( ret< 0 ) {
-       return;
-    }
-
+	ret = SifLoadModule("rom0:LIBSD", 0, NULL);
+	if (ret < 0) {
+		return;
+	}
 }
-long VeryVeryHardFunction (int iterations)
+long VeryVeryHardFunction(int iterations)
 {
 	char *array;
 	int i;
-	long output=0;
-	array =  (char*) malloc (iterations); 
-	
-	for (i=0; i < iterations; i++)
+	long output = 0;
+	array = (char *)malloc(iterations);
+
+	for (i = 0; i < iterations; i++)
 		array[i] = i;
-		
-	for (i=0; i < iterations; i++)
+
+	for (i = 0; i < iterations; i++)
 		output += array[i];
-	
+
 	return (output);
 }
 
-long VeryVeryHardFunctionScratch (int iterations)
+long VeryVeryHardFunctionScratch(int iterations)
 {
 	char *array;
 	int i;
-	long output=0;
-	
-	array =  (char*) malloc (iterations);  // Just to equal algo's
-	
-	array = (char*)0x70000000;
-	for (i=0; i < iterations; i++)
+	long output = 0;
+
+	array = (char *)malloc(iterations);  // Just to equal algo's
+
+	array = (char *)0x70000000;
+	for (i = 0; i < iterations; i++)
 		array[i] = i;
-		
-	for (i=0; i < iterations; i++)
+
+	for (i = 0; i < iterations; i++)
 		output += array[i];
-	
+
 	return (output);
 }
 
-long VeryVeryHardFunctionNoCached (int iterations)
+long VeryVeryHardFunctionNoCached(int iterations)
 {
 	char *array;
 	int i;
-	long output=0;
-	array =  (char*) malloc (iterations) + 0x20000000; 
+	long output = 0;
+	array = (char *)malloc(iterations) + 0x20000000;
 
-	for (i=0; i < iterations; i++)
+	for (i = 0; i < iterations; i++)
 		array[i] = i;
-		
-	for (i=0; i < iterations; i++)
+
+	for (i = 0; i < iterations; i++)
 		output += array[i];
-	
+
 	return (output);
 }
 
-long VeryVeryHardFunctionUnCachedAccelerated (int iterations)
+long VeryVeryHardFunctionUnCachedAccelerated(int iterations)
 {
 	char *array;
 	int i;
-	long output=0;
-	array =  (char*) malloc (iterations) + 0x30000000; 
+	long output = 0;
+	array = (char *)malloc(iterations) + 0x30000000;
 
-	for (i=0; i < iterations; i++)
+	for (i = 0; i < iterations; i++)
 		array[i] = i;
-		
-	for (i=0; i < iterations; i++)
+
+	for (i = 0; i < iterations; i++)
 		output += array[i];
-	
+
 	return (output);
 }
 
@@ -105,92 +104,92 @@ long VeryVeryHardFunctionUnCachedAccelerated (int iterations)
 
 int main()
 {
-  	long output = 0;
-  	
-  	SifInitRpc(0);
+	long output = 0;
+
+	SifInitRpc(0);
 	loadModules();
-//	SjPCM_Init(0);
-	printf ("Yeah... I'm Here\n");
-	
-		initPs2Perf 	();   		// Init The Machine :D
-		mode0Select 	(1,1,1,1);  // We Count on any mode (kernel, user...)
-		mode1Select 	(1,1,1,1);
-		initCounter		(&myCounter0,PC0); // Well... just a shortcut 
-		initCounter		(&myCounter1,PC1);
-		event0Select 	(EV0_ProcessorCycle);		// PC0 Event Counter
-		event1Select    (EV1_DataCacheMiss);	// PC1 Event Counter
-		startPs2Perf 	();   	// Start counters!!! 
-		output = VeryVeryHardFunction (10000);
-		stopPs2Perf 	();		// Stop counters!!!
-		printf ("Output Equals %ld\n",output);
-		
-		// Here counters aren't counting 
-		
-		flushCounter 	(&myCounter0,PC0,0);  // += counter, and counter = 0
-		flushCounter 	(&myCounter1,PC1,0);  // ...
-		printf ("Very Very Hard Test 1\n");
-		printf (DumpInfo (&myCounter0,&myCounter1)); // A simple dumping function...
-/* ===========================================================================================================*/
-		/* Test Another VeryVeryHard Function now using scratchpad */
+	//	SjPCM_Init(0);
+	printf("Yeah... I'm Here\n");
 
-		initCounter		(&myCounter0,PC0); 
-		initCounter		(&myCounter1,PC1);
-		event0Select 	(EV0_ProcessorCycle);		
-		event1Select    (EV1_DataCacheMiss);	
-		printf ("Second Test\n");
-		startPs2Perf 	();   	
-		output = VeryVeryHardFunctionScratch (10000);
-		stopPs2Perf 	();		
-		printf ("Output Equals %ld\n",output);
-		
-		// Here counters aren't counting 
-		
-		flushCounter 	(&myCounter0,PC0,0);  
-		flushCounter 	(&myCounter1,PC1,0);  
-		printf ("Very Very Hard Test 2\n");
-		printf (DumpInfo (&myCounter0,&myCounter1)); 
- 
- /* ===========================================================================================================*/ 
-  /* Test Another VeryVeryHard Function now using no cached memory */
+	initPs2Perf();			  // Init The Machine :D
+	mode0Select(1, 1, 1, 1);  // We Count on any mode (kernel, user...)
+	mode1Select(1, 1, 1, 1);
+	initCounter(&myCounter0, PC0);  // Well... just a shortcut
+	initCounter(&myCounter1, PC1);
+	event0Select(EV0_ProcessorCycle);  // PC0 Event Counter
+	event1Select(EV1_DataCacheMiss);   // PC1 Event Counter
+	startPs2Perf();					   // Start counters!!!
+	output = VeryVeryHardFunction(10000);
+	stopPs2Perf();  // Stop counters!!!
+	printf("Output Equals %ld\n", output);
 
-		initCounter		(&myCounter0,PC0); 
-		initCounter		(&myCounter1,PC1);
-		event0Select 	(EV0_ProcessorCycle);		
-		event1Select    (EV1_DataCacheMiss);	
-		printf ("Third Test\n");
-		startPs2Perf 	();   	
-		output = VeryVeryHardFunctionNoCached (10000);
-		stopPs2Perf 	();		
-		printf ("Output Equals %ld\n",output);
-		
-		// Here counters aren't counting 
-		
-		flushCounter 	(&myCounter0,PC0,0);  
-		flushCounter 	(&myCounter1,PC1,0);  
-		printf ("Very Very Hard Test 3\n");
-		printf (DumpInfo (&myCounter0,&myCounter1)); 
-  
-  /* ===========================================================================================================*/ 
-  /* Test Another VeryVeryHard Function now using uncached accelerated memory */
+	// Here counters aren't counting
 
-		initCounter		(&myCounter0,PC0); 
-		initCounter		(&myCounter1,PC1);
-		event0Select 	(EV0_ProcessorCycle);		
-		event1Select    (EV1_DataCacheMiss);	
-		printf ("Fourth Test\n");
-		startPs2Perf 	();   	
-		output = VeryVeryHardFunctionUnCachedAccelerated (10000);
-		stopPs2Perf 	();		
-		printf ("Output Equals %ld\n",output);
-		
-		// Here counters aren't counting 
-		
-		flushCounter 	(&myCounter0,PC0,0);  
-		flushCounter 	(&myCounter1,PC1,0);  
-		printf ("Very Very Hard Test 4\n");
-		printf (DumpInfo (&myCounter0,&myCounter1)); 
+	flushCounter(&myCounter0, PC0, 0);  // += counter, and counter = 0
+	flushCounter(&myCounter1, PC1, 0);  // ...
+	printf("Very Very Hard Test 1\n");
+	printf(DumpInfo(&myCounter0, &myCounter1));  // A simple dumping function...
+												 /* ===========================================================================================================*/
+	/* Test Another VeryVeryHard Function now using scratchpad */
 
-  	// Zzzzzz...
-  	SleepThread();
-  	return EXIT_SUCCESS;
+	initCounter(&myCounter0, PC0);
+	initCounter(&myCounter1, PC1);
+	event0Select(EV0_ProcessorCycle);
+	event1Select(EV1_DataCacheMiss);
+	printf("Second Test\n");
+	startPs2Perf();
+	output = VeryVeryHardFunctionScratch(10000);
+	stopPs2Perf();
+	printf("Output Equals %ld\n", output);
+
+	// Here counters aren't counting
+
+	flushCounter(&myCounter0, PC0, 0);
+	flushCounter(&myCounter1, PC1, 0);
+	printf("Very Very Hard Test 2\n");
+	printf(DumpInfo(&myCounter0, &myCounter1));
+
+	/* ===========================================================================================================*/
+	/* Test Another VeryVeryHard Function now using no cached memory */
+
+	initCounter(&myCounter0, PC0);
+	initCounter(&myCounter1, PC1);
+	event0Select(EV0_ProcessorCycle);
+	event1Select(EV1_DataCacheMiss);
+	printf("Third Test\n");
+	startPs2Perf();
+	output = VeryVeryHardFunctionNoCached(10000);
+	stopPs2Perf();
+	printf("Output Equals %ld\n", output);
+
+	// Here counters aren't counting
+
+	flushCounter(&myCounter0, PC0, 0);
+	flushCounter(&myCounter1, PC1, 0);
+	printf("Very Very Hard Test 3\n");
+	printf(DumpInfo(&myCounter0, &myCounter1));
+
+	/* ===========================================================================================================*/
+	/* Test Another VeryVeryHard Function now using uncached accelerated memory */
+
+	initCounter(&myCounter0, PC0);
+	initCounter(&myCounter1, PC1);
+	event0Select(EV0_ProcessorCycle);
+	event1Select(EV1_DataCacheMiss);
+	printf("Fourth Test\n");
+	startPs2Perf();
+	output = VeryVeryHardFunctionUnCachedAccelerated(10000);
+	stopPs2Perf();
+	printf("Output Equals %ld\n", output);
+
+	// Here counters aren't counting
+
+	flushCounter(&myCounter0, PC0, 0);
+	flushCounter(&myCounter1, PC1, 0);
+	printf("Very Very Hard Test 4\n");
+	printf(DumpInfo(&myCounter0, &myCounter1));
+
+	// Zzzzzz...
+	SleepThread();
+	return EXIT_SUCCESS;
 }
--- ee/ps2Perf/ps2Perf.c
+++ ee/ps2Perf/ps2Perf.c
@@ -38,49 +38,68 @@
 #include <string.h>
 #include <malloc.h>
 #include <fileio.h>
-#define SENDTOPCCR(x)	__asm __volatile (" mtps %0,0 \n\t SYNC.P" : : "r"(x) : "memory");
-#define GETFROMPCCR(x)	__asm __volatile (" mfps %0,0 " : "=r"(x) :"r"(x) : "memory"); 
-#define CLEARPCCR	__asm __volatile (" mtps $0,0 \n\t SYNC.P" : : : "memory");
-#define CLEARPC0	__asm __volatile (" mtpc $0,0 \n\t SYNC.P" : : : "memory");
-#define GETPC0(x)	__asm __volatile (" mfpc %0,0 \n\t SYNC.P" :"=r"(x) :"r"(x) : "memory");
-#define CLEARPC1	__asm __volatile (" mtpc $0,1 \n\t SYNC.P" : : : "memory");
-#define GETPC1(x)	__asm __volatile (" mfpc %0,1 \n\t SYNC.P" :"=r"(x) :"r"(x) : "memory");
+#define SENDTOPCCR(x) __asm __volatile(" mtps %0,0 \n\t SYNC.P" \
+									   :                        \
+									   : "r"(x)                 \
+									   : "memory");
+#define GETFROMPCCR(x) __asm __volatile(" mfps %0,0 " \
+										: "=r"(x)     \
+										: "r"(x)      \
+										: "memory");
+#define CLEARPCCR __asm __volatile(" mtps $0,0 \n\t SYNC.P" \
+								   :                        \
+								   :                        \
+								   : "memory");
+#define CLEARPC0 __asm __volatile(" mtpc $0,0 \n\t SYNC.P" \
+								  :                        \
+								  :                        \
+								  : "memory");
+#define GETPC0(x) __asm __volatile(" mfpc %0,0 \n\t SYNC.P" \
+								   : "=r"(x)                \
+								   : "r"(x)                 \
+								   : "memory");
+#define CLEARPC1 __asm __volatile(" mtpc $0,1 \n\t SYNC.P" \
+								  :                        \
+								  :                        \
+								  : "memory");
+#define GETPC1(x) __asm __volatile(" mfpc %0,1 \n\t SYNC.P" \
+								   : "=r"(x)                \
+								   : "r"(x)                 \
+								   : "memory");
 
-void initPs2Perf ()
+void initPs2Perf()
 {
-				
-		CLEARPCCR; // Clean PCCR Control Register (stop it)
-		
-	
+
+	CLEARPCCR;  // Clean PCCR Control Register (stop it)
 }
 
-void startPs2Perf ()
-{ 
+void startPs2Perf()
+{
 	PCCR_Register reg_temp;
-	
-	GETFROMPCCR (reg_temp);
-	reg_temp.CTE = 1;		// Send to Start
-	SENDTOPCCR (reg_temp);
+
+	GETFROMPCCR(reg_temp);
+	reg_temp.CTE = 1;  // Send to Start
+	SENDTOPCCR(reg_temp);
 }
 
 
-void stopPs2Perf ()
-{ 
+void stopPs2Perf()
+{
 	PCCR_Register reg_temp;
-	
-	GETFROMPCCR (reg_temp);
-	reg_temp.CTE = 0;		// Send to Stop
-	SENDTOPCCR (reg_temp);
+
+	GETFROMPCCR(reg_temp);
+	reg_temp.CTE = 0;  // Send to Stop
+	SENDTOPCCR(reg_temp);
 }
 
 
-void initCounter (void * counters,char type)
+void initCounter(void *counters, char type)
 {
 
 	if (type == PC0)
-		memset (counters,0,sizeof(Counters0));
+		memset(counters, 0, sizeof(Counters0));
 	else if (type == PC1)
-		memset (counters,0,sizeof(Counters1));
+		memset(counters, 0, sizeof(Counters1));
 }
 
 
@@ -88,169 +107,227 @@ void initCounter (void * counters,char type)
 	Stop PC's, read PCx, add to correct counter, put PCx = 0, and restart PC's
 	Type = PC0 or PC1
 */
-void flushCounter (void * counters,char type,char reStart)
+void flushCounter(void *counters, char type, char reStart)
 {
 	Counters0 *c0;
 	Counters1 *c1;
 	long value = 0;
 	PCCR_Register reg_temp;
-	GETFROMPCCR (reg_temp);
-	reg_temp.CTE = 0;  		// Stop Counters
-	SENDTOPCCR (reg_temp);
+	GETFROMPCCR(reg_temp);
+	reg_temp.CTE = 0;  // Stop Counters
+	SENDTOPCCR(reg_temp);
 
-	if (type == PC0)
-	{
+	if (type == PC0) {
 		c0 = counters;
 		GETPC0(value);
-		
+
 		CLEARPC0;
-		switch (reg_temp.EVENT0)
-		{
-		case EV0_ProcessorCycle 			:	c0->ProcessorCycle				+=value;break;
-		case EV0_SingleInstructionIssue		:	c0->SingleInstructionIssue		+=value;break;
-		case EV0_BranchIssued				:	c0->BranchIssued				+=value;break;
-		case EV0_BTACmiss					:	c0->BTACmiss					+=value;break;
-		case EV0_ITLBmiss					:	c0->ITLBmiss					+=value;break;	
-		case EV0_InstructionCacheMiss		:	c0->InstructionCacheMiss		+=value;break;
-		case EV0_AccessToDTLB				:	c0->AccessToDTLB				+=value;break;
-		case EV0_nonBlockingLoad			:	c0->nonBlockingLoad				+=value;break;
-		case EV0_WBBSingleRequest			:	c0->WBBSingleRequest			+=value;break;
-		case EV0_WBBBurstRequest			:	c0->WBBBurstRequest				+=value;break;		
-		case EV0_CPUAdressBusBusy			:	c0->CPUAdressBusBusy			+=value;break;	
-		case EV0_InstructionCompleted		:	c0->InstructionCompleted		+=value;break;	
-		case EV0_NonBDSInstrucionCompleted	: 	c0->NonBDSInstrucionCompleted	+=value;break;		
-		case EV0_COP2InstructionCompleted	: 	c0->COP2InstructionCompleted	+=value;break;
-		case EV0_LoadCompleted				: 	c0->LoadCompleted				+=value;break;
-		default : CLEARPC0;
-		}	
-		
-	}	
-	else if (type == PC1)
-	{
+		switch (reg_temp.EVENT0) {
+			case EV0_ProcessorCycle:
+				c0->ProcessorCycle += value;
+				break;
+			case EV0_SingleInstructionIssue:
+				c0->SingleInstructionIssue += value;
+				break;
+			case EV0_BranchIssued:
+				c0->BranchIssued += value;
+				break;
+			case EV0_BTACmiss:
+				c0->BTACmiss += value;
+				break;
+			case EV0_ITLBmiss:
+				c0->ITLBmiss += value;
+				break;
+			case EV0_InstructionCacheMiss:
+				c0->InstructionCacheMiss += value;
+				break;
+			case EV0_AccessToDTLB:
+				c0->AccessToDTLB += value;
+				break;
+			case EV0_nonBlockingLoad:
+				c0->nonBlockingLoad += value;
+				break;
+			case EV0_WBBSingleRequest:
+				c0->WBBSingleRequest += value;
+				break;
+			case EV0_WBBBurstRequest:
+				c0->WBBBurstRequest += value;
+				break;
+			case EV0_CPUAdressBusBusy:
+				c0->CPUAdressBusBusy += value;
+				break;
+			case EV0_InstructionCompleted:
+				c0->InstructionCompleted += value;
+				break;
+			case EV0_NonBDSInstrucionCompleted:
+				c0->NonBDSInstrucionCompleted += value;
+				break;
+			case EV0_COP2InstructionCompleted:
+				c0->COP2InstructionCompleted += value;
+				break;
+			case EV0_LoadCompleted:
+				c0->LoadCompleted += value;
+				break;
+			default:
+				CLEARPC0;
+		}
+
+	} else if (type == PC1) {
 		c1 = counters;
 		GETPC1(value);
-		
+
 		CLEARPC1;
-		switch (reg_temp.EVENT1)
-		{
-		case EV1_LowOrderBranchIssued		:	c1->LowOrderBranchIssued			+=value; break;
-		case EV1_ProcessorCycle   			:	c1->ProcessorCycle					+=value;break;
-		case EV1_DualInstructionIssue		:	c1->DualInstructionIssue			+=value;break;
-		case EV1_BranchMispredicted			:	c1->BranchMispredicted				+=value;break;
-		case EV1_TLBmiss					:	c1->TLBmiss							+=value;break;	
-		case EV1_DTLBmiss					:	c1->DTLBmiss						+=value;break;
-		case EV1_DataCacheMiss				:	c1->DataCacheMiss					+=value;break;
-		case EV1_WBBSingleRequestUnavailable:	c1->WBBSingleRequestUnavailable		+=value;break;
-		case EV1_WBBBurstRequestUnavailable	:	c1->WBBBurstRequestUnavailable		+=value;break;
-		case EV1_WBBBurstRequestAlmostFull	:	c1->WBBBurstRequestAlmostFull		+=value;break;		
-		case EV1_WBBBurstRequestFull		:	c1->WBBBurstRequestFull				+=value;break;	
-		case EV1_CPUDataBusBusy				:	c1->CPUDataBusBusy					+=value;break;	
-		case EV1_InstructionCompleted		: 	c1->InstructionCompleted			+=value;break;		
-		case EV1_NonBDSInstrucionCompleted	: 	c1->NonBDSInstrucionCompleted		+=value;break;
-		case EV1_COP1InstructionCompleted	: 	c1->COP1InstructionCompleted		+=value;break;
-		case EV1_StoreCompleted				:   c1->StoreCompleted					+=value;break;
-		default : CLEARPC1;
-		}	
+		switch (reg_temp.EVENT1) {
+			case EV1_LowOrderBranchIssued:
+				c1->LowOrderBranchIssued += value;
+				break;
+			case EV1_ProcessorCycle:
+				c1->ProcessorCycle += value;
+				break;
+			case EV1_DualInstructionIssue:
+				c1->DualInstructionIssue += value;
+				break;
+			case EV1_BranchMispredicted:
+				c1->BranchMispredicted += value;
+				break;
+			case EV1_TLBmiss:
+				c1->TLBmiss += value;
+				break;
+			case EV1_DTLBmiss:
+				c1->DTLBmiss += value;
+				break;
+			case EV1_DataCacheMiss:
+				c1->DataCacheMiss += value;
+				break;
+			case EV1_WBBSingleRequestUnavailable:
+				c1->WBBSingleRequestUnavailable += value;
+				break;
+			case EV1_WBBBurstRequestUnavailable:
+				c1->WBBBurstRequestUnavailable += value;
+				break;
+			case EV1_WBBBurstRequestAlmostFull:
+				c1->WBBBurstRequestAlmostFull += value;
+				break;
+			case EV1_WBBBurstRequestFull:
+				c1->WBBBurstRequestFull += value;
+				break;
+			case EV1_CPUDataBusBusy:
+				c1->CPUDataBusBusy += value;
+				break;
+			case EV1_InstructionCompleted:
+				c1->InstructionCompleted += value;
+				break;
+			case EV1_NonBDSInstrucionCompleted:
+				c1->NonBDSInstrucionCompleted += value;
+				break;
+			case EV1_COP1InstructionCompleted:
+				c1->COP1InstructionCompleted += value;
+				break;
+			case EV1_StoreCompleted:
+				c1->StoreCompleted += value;
+				break;
+			default:
+				CLEARPC1;
+		}
 	}
-	if (reStart==1) reg_temp.CTE = 1;  		// ReStart Counters
-	SENDTOPCCR (reg_temp);
-	
+	if (reStart == 1)
+		reg_temp.CTE = 1;  // ReStart Counters
+	SENDTOPCCR(reg_temp);
 }
 
 /* It will stop counters, and clear PC0, (re)Start Manually */
-void event0Select 	(char event0)
+void event0Select(char event0)
 {
 	PCCR_Register reg_temp;
-	GETFROMPCCR (reg_temp);
-	reg_temp.CTE = 0;  		// Stop Counters
-	SENDTOPCCR (reg_temp);
-	
+	GETFROMPCCR(reg_temp);
+	reg_temp.CTE = 0;  // Stop Counters
+	SENDTOPCCR(reg_temp);
+
 	reg_temp.EVENT0 = event0;
 	CLEARPC0;
-	SENDTOPCCR (reg_temp);
+	SENDTOPCCR(reg_temp);
 }
 
 /* It will stop counters, and clear PC1, (re)Start Manually */
-void event1Select 	(char event1)
+void event1Select(char event1)
 {
 	PCCR_Register reg_temp;
-	GETFROMPCCR (reg_temp);
-	reg_temp.CTE = 0;  		// Stop Counters
-	SENDTOPCCR (reg_temp);
-	
+	GETFROMPCCR(reg_temp);
+	reg_temp.CTE = 0;  // Stop Counters
+	SENDTOPCCR(reg_temp);
+
 	reg_temp.EVENT1 = event1;
 	CLEARPC1;
-	SENDTOPCCR (reg_temp);
+	SENDTOPCCR(reg_temp);
 }
 
 /* It will stop counters, (re)Start Manually */
-void mode0Select 	(char level1,char kernel,char supervisor, char user)
+void mode0Select(char level1, char kernel, char supervisor, char user)
 {
 	PCCR_Register reg_temp;
-	GETFROMPCCR (reg_temp);	
-	reg_temp.CTE = 0;  		// Stop Counters
-	SENDTOPCCR (reg_temp);
+	GETFROMPCCR(reg_temp);
+	reg_temp.CTE = 0;  // Stop Counters
+	SENDTOPCCR(reg_temp);
 	reg_temp.EXL0 = level1;
 	reg_temp.K0 = kernel;
 	reg_temp.S0 = supervisor;
 	reg_temp.U0 = user;
-	
-	SENDTOPCCR (reg_temp);
+
+	SENDTOPCCR(reg_temp);
 }
 
 
 /* It will stop counters, (re)Start Manually */
-void mode1Select 	(char level1,char kernel,char supervisor, char user)
+void mode1Select(char level1, char kernel, char supervisor, char user)
 {
 	PCCR_Register reg_temp;
-	GETFROMPCCR (reg_temp);	
-	reg_temp.CTE = 0;  		// Stop Counters
-	SENDTOPCCR (reg_temp);
+	GETFROMPCCR(reg_temp);
+	reg_temp.CTE = 0;  // Stop Counters
+	SENDTOPCCR(reg_temp);
 	reg_temp.EXL1 = level1;
 	reg_temp.K1 = kernel;
 	reg_temp.S1 = supervisor;
 	reg_temp.U1 = user;
-	
-	SENDTOPCCR (reg_temp);
+
+	SENDTOPCCR(reg_temp);
 }
 
-char * DumpInfo (Counters0 *c0,Counters1 *c1)
+char *DumpInfo(Counters0 *c0, Counters1 *c1)
 {
-	char * output;
-	output = (char *) malloc (sizeof(char)*1024);
-	sprintf (output,"\n[PC0] C1: %ld C2: %ld C3: %ld C4: %ld C5: %ld C6: %ld C7: %ld\nC8: %ld C9: %ld C10: %ld C11: %ld C12: %ld C13: %ld C14: %ld C15: %ld \n[PC1] C0: %ld C1: %ld C2: %ld C3: %ld C4: %ld C5: %ld C6: %ld C7: %ld \nC8: %ld C9: %ld C10: %ld C11: %ld C12: %ld C13: %ld C14: %ld C15: %ld\n",
-	c0->ProcessorCycle,   				
-	c0->SingleInstructionIssue,		
-	c0->BranchIssued,					
-	c0->BTACmiss,						
-	c0->ITLBmiss,						
-	c0->InstructionCacheMiss,			
-	c0->AccessToDTLB,					
-	c0->nonBlockingLoad,				
-	c0->WBBSingleRequest,					
-	c0->WBBBurstRequest,				
-	c0->CPUAdressBusBusy,				
-	c0->InstructionCompleted,		
-	c0->NonBDSInstrucionCompleted,		
-	c0->COP2InstructionCompleted,		
-	c0->LoadCompleted,
-	c1->LowOrderBranchIssued,			
-	c1->ProcessorCycle,   				
-	c1->DualInstructionIssue,			
-	c1->BranchMispredicted,			
-	c1->TLBmiss,					
-	c1->DTLBmiss,						
-	c1->DataCacheMiss,					
-	c1->WBBSingleRequestUnavailable,	
-	c1->WBBBurstRequestUnavailable,	
-	c1->WBBBurstRequestAlmostFull,		
-	c1->WBBBurstRequestFull,			
-	c1->CPUDataBusBusy,				
-	c1->InstructionCompleted,		
-	c1->NonBDSInstrucionCompleted,		
-	c1->COP1InstructionCompleted,		
-	c1->StoreCompleted);			
+	char *output;
+	output = (char *)malloc(sizeof(char) * 1024);
+	sprintf(output, "\n[PC0] C1: %ld C2: %ld C3: %ld C4: %ld C5: %ld C6: %ld C7: %ld\nC8: %ld C9: %ld C10: %ld C11: %ld C12: %ld C13: %ld C14: %ld C15: %ld \n[PC1] C0: %ld C1: %ld C2: %ld C3: %ld C4: %ld C5: %ld C6: %ld C7: %ld \nC8: %ld C9: %ld C10: %ld C11: %ld C12: %ld C13: %ld C14: %ld C15: %ld\n",
+			c0->ProcessorCycle,
+			c0->SingleInstructionIssue,
+			c0->BranchIssued,
+			c0->BTACmiss,
+			c0->ITLBmiss,
+			c0->InstructionCacheMiss,
+			c0->AccessToDTLB,
+			c0->nonBlockingLoad,
+			c0->WBBSingleRequest,
+			c0->WBBBurstRequest,
+			c0->CPUAdressBusBusy,
+			c0->InstructionCompleted,
+			c0->NonBDSInstrucionCompleted,
+			c0->COP2InstructionCompleted,
+			c0->LoadCompleted,
+			c1->LowOrderBranchIssued,
+			c1->ProcessorCycle,
+			c1->DualInstructionIssue,
+			c1->BranchMispredicted,
+			c1->TLBmiss,
+			c1->DTLBmiss,
+			c1->DataCacheMiss,
+			c1->WBBSingleRequestUnavailable,
+			c1->WBBBurstRequestUnavailable,
+			c1->WBBBurstRequestAlmostFull,
+			c1->WBBBurstRequestFull,
+			c1->CPUDataBusBusy,
+			c1->InstructionCompleted,
+			c1->NonBDSInstrucionCompleted,
+			c1->COP1InstructionCompleted,
+			c1->StoreCompleted);
 
 	return (output);
-	
 }
